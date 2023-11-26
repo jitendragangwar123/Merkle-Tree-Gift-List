982fba1e16d04c9123f3143e26523959c07ee122
jitendra <gangwarjitendra2498@gmail.com>
2023-11-26 19:58:16 +0530

    feat:verifyProof function added

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const verifyProof = require('../utils/verifyProof');
-
+const niceList = require('../utils/niceList.json')
+const MerkleTree = require("../utils/MerkleTree")
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
+const merkleTree = new MerkleTree(niceList)
+const root = merkleTree.getRoot()
+
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = root;
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
-
+  const {name , proof} = body;
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(proof,name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
